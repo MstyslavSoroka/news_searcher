525ef7d84ecaed07a8816ec3506577596e261c71
MstyslavSoroka <mstyslav2010@gmail.com>
2025-02-02 10:17:38 +0000

    news search

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
-fetch('https://restcountries.com/v3.1/name/ukraine').then((result) => {
-  console.log(result.json());
+const input = document.getElementById('input');
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const request = input.value.trim();
+  if (!request) {
+    alert('enter key word');
+    return;
+  }
+  fetch(
+    `https://newsapi.org/v2/everything?q=${request}&apiKey=4988d0376de8410b9940549c5ab8400e`,
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      const field = document.getElementById('field');
+      field.innerHTML = '';
+      console.log(data);
+      data.articles.forEach((article) => {
+        const articleDiv = document.createElement('div');
+        articleDiv.classList.add('article');
+        articleDiv.innerHTML = `
+            <h3>${article.title}</h3>
+            <p>${article.description}</p>
+            <a href="${article.url}">Read more</a>
+        `;
+        field.appendChild(articleDiv);
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+      field.innerHTML = `
+        <p>Error</p>
+      `;
+    });
+  input.value = '';
 });
